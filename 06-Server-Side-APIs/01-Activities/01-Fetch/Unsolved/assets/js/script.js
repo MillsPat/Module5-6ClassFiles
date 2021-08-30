666e70a31813b659b0a295f9195ea908512fb904
MillsPat <millspat71@gmail.com>
2021-08-30 18:34:07 -0400

    New Commit 5

--- 06-Server-Side-APIs/01-Activities/01-Fetch/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/01-Fetch/Unsolved/assets/js/script.js
@@ -1,2 +1,17 @@
 // Write a fetch request to the Giphy API
 // Then log the response in the console
+let giphyAPI = "https://api.giphy.com/v1/gifs/trending?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN";
+
+		function setup(){
+
+			fetch(giphyAPI)
+			.then(response => {
+				return response.json();
+			})
+			.then(json => {
+				createImg(json.data[0].images.original.url);
+			})
+			.catch(err => console.log(err));
+
+		}
+      
